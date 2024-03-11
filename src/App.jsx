53bd705133a3f275e661687e7e1e796d9e07e310
titopope@hotmail.com <titopope@hotmail.com>
2024-03-11 11:44:43 +0100

    antes de clase

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,55 @@
+import { useState } from 'react'
 import './App.css'
 
-function App() {
+/*function App() {
  //Aquí tu código
-}
+    const [name, setName] = useState('Sofia');
+    const [newName, setNewName] = useState('');
+    const teacherChange = (newTeacherName) => {
+        setName(newTeacherName);
+    };
+    return (
+        <div>
+            <h2>Teacher name: {name}</h2>
+            <ul>
+                <li onClick={() => teacherChange('Data') }>Data</li>
+                <li onClick={() => teacherChange('Reyes')}>Reyes</li>
+                <li onClick={() => teacherChange('Yolanda')}>Yolanda</li>
+            </ul>
+        </div>
+    )
+}*/
+
+function App() {
+    const [name, setName] = useState('Sofia');
+    const [newName, setNewName] = useState('');
+  
+    const changeName = (e) => {
+      e.preventDefault(); 
+      if (newName.trim() !== '') {
+        setName(newName);
+        setNewName('');
+      }
+    };
+  
+    const nameInputChange = (e) => {
+      setNewName(e.target.value);
+    };
+  
+    return (
+      <div>
+        <h2>Teacher Name: {name}</h2>
+        <form onSubmit={changeName}>
+          <input
+            type="text"
+            value={newName}
+            onChange={nameInputChange}
+            placeholder="Add a name"
+          />
+          <button type="submit">Add</button>
+        </form>
+      </div>
+    );
+  }
 
 export default App
